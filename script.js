f8760653d0f9fe860e0ed3f896f4047d98271053
Chris Wakley <chris.wakley666@gmail.com>
2022-10-10 22:35:40 +0100

    shuffle and game start functions working. had to move runes.js to index.html.

--- script.js
+++ script.js
@@ -1,60 +1,57 @@
-import { runesArray } from "./runes.js";
+// import { runesArray } from "./runes.js";
 
-console.log(runesArray);
+// console.log(runesArray);
 const gameContainer = document.querySelector(".game__container");
-const cards = document.querySelectorAll(".card");
+const cards = Array.from(document.querySelectorAll(".card"));
+const startGame = document.querySelector(".pannel__button--start");
+const resetGame = document.querySelector(".pannel__button--reset");
+// let cardOne, cardTwo;
+// let disableDeck = false;
 
-let cardOne, cardTwo;
-let disableDeck = false;
-
-
-const showCards = () => {
-    runesArray.forEach((runes) => {
-        gameContainer.innerHTML +=
-        `<div class= "card">
-            <div class= "card__front">
-                <img src="${runes.imagesrcfront}" class= "card__imagefront">
-                <div class= "card__content">${(runes.name)}</div>
-                </div>
-            <div class= "card__back">
-                <img src="${runes.imagesrcback}" class= "card__imageback">
-                </div>
-            </div>`;
-    })
+//THIS SHUFFLES THE CARDS ON PAGE LOAD. need to edit to shuffle on click start.
+const shuffle = (e) => {
+    const children = [...gameContainer.children];
+    let currentIndex = children.length, temporaryValue, randomIndex;
+    while (0!== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = children[currentIndex];
+        children[currentIndex] = children[randomIndex];
+        children[randomIndex] = temporaryValue;
+    }
+    gameContainer.innerHTML=""
+    children.forEach(child => gameContainer.append(child))
 };
-showCards(runesArray);
+shuffle();
+startGame.addEventListener("click", shuffle);
 
-const card = document.querySelector(".card");
-const flipCard = () => {
-    card.classList.toggle("flipCard");
+
+// const showCards = () => {
+//     runesArray.forEach((runes) => {
+//         gameContainer.innerHTML +=
+//         `<div class= "card">
+//             <div class= "card__front">
+//                 <img src="${runes.imagesrcfront}" class= "card__imagefront">
+//                 <div class= "card__content">${(runes.name)}</div>
+//                 </div>
+//             <div class= "card__back">
+//                 <img src="${runes.imagesrcback}" class= "card__imageback">
+//                 </div>
+//             </div>`;
+//     })
+// };
+// showCards(runesArray);
+
+////THIS WORKS! need to make each card a block element so images and text cant be flipped.
+const flipCard = (e) => {
+    e.target.classList.toggle("flipCard");
+    console.log(e.target);
+};
+// cards.forEach((card) => {
+//     card.addEventListener("click", flipCard)
+// });
+
+
+for(let i = 0; i < cards.length; i++){
+    cards[i].addEventListener("click", flipCard);
 }
-card.addEventListener("click", flipCard);
-
-
-
-// const flipCard = ({target:clickedCard}) => {
-//     if(cardOne !== clickedCard && !disableDeck) {
-//         clickedCard.classList.add(".flipCard");
-//         if(!cardOne) {
-//             return cardOne = clickedCard;
-//         }
-//         cardTwo = clickedCard;
-//         disableDeck = true;
-//         let getCardOnePair = (runesArray) => {
-//             const cardPair = runesArray.map((runesArray) => {
-//             return runesArray[0].id;
-//             });
-//             return cardPair;
-//             }
-//         }
-// }
-
-
-
-// console.log(cardTwo);
-// const flipCard = () => {
-//     cards.forEach (card => {
-//         card.classList.toggle(".flipCard")
-//         card.addEventListener("click", flipCard)
-//     });
-// }
